f61758a4da3dbda9086a2ad31f0789e5a4117284
Per Flitig <per.flitig@gmail.com>
2018-08-13 10:05:21 +0200

    Add tests and finished code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-'use strict';
+'use strict'
 
+function primeFactors (n) {
+  let primes = []
+  let candidate = 2
 
-exports = {
+  while (n > candidate) {
+    while (n % candidate === 0) {
+      primes.push(candidate)
+      n = n / candidate
+    }
+    candidate++
+  }
 
+  if (n > 1) {
+    primes.push(n)
+  }
+
+  return primes
 }
+
+exports.primeFactors = primeFactors
--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
+/* eslint-env mocha */
+'use strict'
 
-'use strict';
-
-var assert = require('assert');
+var assert = require('assert')
 var lib = require('../src/index.js')
 
 /*
@@ -24,8 +24,64 @@ var lib = require('../src/index.js')
   assert.throws(block[, error][, message])
 */
 
-describe('Library', function() {
-  it('should be defined', function() {
-    assert(typeof lib !== 'undefined');
-  });
-});
+describe('Prime Factors', function () {
+  it('1 should return []', function () {
+    let expected = []
+    let actual = lib.primeFactors(1)
+    assert.deepEqual(expected, actual)
+  })
+
+  it('2 should return [2]', function () {
+    let expected = [2]
+    let actual = lib.primeFactors(2)
+    assert.deepEqual(expected, actual)
+  })
+
+  it('3 should return [3]', function () {
+    let expected = [3]
+    let actual = lib.primeFactors(3)
+    assert.deepEqual(expected, actual)
+  })
+
+  it('4 should return [2,2]', function () {
+    let expected = [2, 2]
+    let actual = lib.primeFactors(4)
+    assert.deepEqual(expected, actual)
+  })
+
+  it('6 should return [2,3]', function () {
+    let expected = [2, 3]
+    let actual = lib.primeFactors(6)
+    assert.deepEqual(expected, actual)
+  })
+
+  it('8 should return [2,2,2]', function () {
+    let expected = [2, 2, 2]
+    let actual = lib.primeFactors(8)
+    assert.deepEqual(expected, actual)
+  })
+
+  it('9 should return [3,3]', function () {
+    let expected = [3, 3]
+    let actual = lib.primeFactors(9)
+    assert.deepEqual(expected, actual)
+  })
+
+  it('10 should return [2,5]', function () {
+    let expected = [2, 5]
+    let actual = lib.primeFactors(10)
+    assert.deepEqual(expected, actual)
+  })
+
+  it('100 should return [2,2,5,5]', function () {
+    let expected = [2, 2, 5, 5]
+    let actual = lib.primeFactors(100)
+    assert.deepEqual(expected, actual)
+  })
+
+  it('256 should return [2,2,2,2,2,2,2,2]', function () {
+    let expected = [2, 2, 2, 2, 2, 2, 2, 2]
+    let actual = lib.primeFactors(256)
+    assert.deepEqual(expected, actual)
+  })
+})
